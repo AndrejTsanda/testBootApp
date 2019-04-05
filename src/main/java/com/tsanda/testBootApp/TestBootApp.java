13a22c60aa59faee194931cb2f3825e27711e163
AndrejTsanda <tsanda.andrej@gmail.com>
2019-04-05 15:30:14 +0300

    Initial commit

---
+++ src/main/java/com/tsanda/testBootApp/TestBootApp.java
@@ -0,0 +1,12 @@
+package com.tsanda.testBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBootApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBootApp.class, args);
+    }
+}
